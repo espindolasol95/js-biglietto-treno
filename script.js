5c7251c960fb43c91f5d2032f77479f4fe536784
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-01 17:17:12 +0200

    if_else_sconto

--- script.js
+++ script.js
@@ -17,3 +17,14 @@ if (isNaN(km) || isNaN(eta) || km <= 0 || eta <= 0) {
 //prezzo iniziale
 const prezzoBase = km *0.21;
 
+//calcoliamo lo sconto
+
+let sconto =0;     
+//applichiamo un 20% 
+if (eta < 18) {
+    sconto = 0.20;
+}
+//applichiamo un 40%
+else if (eta > 65){
+    sconto = 0.40;
+}
