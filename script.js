ef66fbe2e409ad5e12e299882ca70cd79141b8a3
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-01 17:35:09 +0200

    prezzo_finale

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const eta = parseInt(prompt("Quanti anni hai?"));
 //input 
 if (isNaN(km) || isNaN(eta) || km <= 0 || eta <= 0) {
   alert(" Inserisci valori validi per chilometri e età.");
-} 
+} else{
     
 //prezzo iniziale
 const prezzoBase = km *0.21;
@@ -27,4 +27,14 @@ if (eta < 18) {
 //applichiamo un 40%
 else if (eta > 65){
     sconto = 0.40;
+}
+
+//prezzi gia scontati
+const prezzoFinale = (prezzoBase * (1 - sconto)).toFixed(2); //il numero(2) dice di lasciare due numeri dopo l'arrotondamento
+
+
+
+//PREZZO FINALE
+alert (`Il prezzo del biglietto è: €${prezzoFinale}`);
+
 }
