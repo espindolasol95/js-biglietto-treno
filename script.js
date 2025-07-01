234b86543663902b0dedf379cae00e5e59069376
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-01 16:23:14 +0200

    if

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function calcolaPrezzo () {
   const eta = persint (document.getElementById ("età").value ); // perseIntla età deve essere un numero intero
 
 }
+
+if (NaN(km) || NaN (età) ||km <= 0 ||età <=0) {
+    document. getElementById ("risultato") . innerText = "inserisci valori validi";
+    return; 
+}
+
